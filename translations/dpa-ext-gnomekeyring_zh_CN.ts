a145c37e002d83e0204b09dfca3df613bfca659d
chenwei <chenwei@uniontech.com>
2020-08-26 09:06:56 +0800

    Revert "fix: 密码验证框提示文案不正确"
    This reverts commit 19416e03e349639562bbb13302755d49fa05a73d.
    
    Change-Id: I4ada2736005043196734a66ce487e61d380d071b

--- translations/dpa-ext-gnomekeyring_zh_CN.ts
+++ translations/dpa-ext-gnomekeyring_zh_CN.ts
@@ -1,18 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>GnomeKeyringExtention</name>
     <message>
-        <location filename="../gnomekeyringextention.cpp" line="76"/>
+        <location filename="../gnomekeyringextention.cpp" line="57"/>
         <source>Empty keyring password</source>
         <translation>清空钥匙环密码</translation>
     </message>
     <message>
-        <location filename="../gnomekeyringextention.cpp" line="76"/>
-        <source>To simplify system verification, it is recommended to empty the keyring password</source>
-        <translation>为简化验证步骤，建议清空钥匙环密码</translation>
-    </message>
-    <message>
-        <location filename="../gnomekeyringextention.cpp" line="78"/>
+        <location filename="../gnomekeyringextention.cpp" line="59"/>
         <source>Restore keyring password</source>
         <translation>恢复钥匙环密码</translation>
     </message>
